ce31552f2f0c6e1e266b03f92e853e814059b223
nlselig <66334176+nlselig@users.noreply.github.com>
2020-07-29 13:53:53 -0400

    timer implemented

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,19 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var count = 60
+    //egg times dictionary
+    let eggTimes = ["Soft":  300, "Medium": 420 , "Hard": 720]
     
-    //dictionary
-    let eggTimes = ["Soft":  5, "Medium": 7, "Hard": 12]
+    //timer variables
+    var secondsRemaining = 60
+    var timer = Timer()
     
+    //outlets
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    //Buttons
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        //make sure timer isn't running
+        timer.invalidate()
         let hardness = sender.currentTitle! //this will never fail, never be nil
         let result = eggTimes[hardness]!
         print(hardness)
-        print(result)
+        //print(result)
+        
+       secondsRemaining = result
+        
+        //counts down according to hardness selected
+       timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector (updateTimer), userInfo: nil, repeats: true )
     }
     
+    //helper function for the timer
+    @objc func updateTimer() {
+           if (secondsRemaining > 0){
+               print("\(secondsRemaining) seconds.")
+               secondsRemaining -= 1
+           }
+           else {
+            timer.invalidate()
+            statusLabel.text = "DONE!"
+            }
+       }
     
-    
-
 }
